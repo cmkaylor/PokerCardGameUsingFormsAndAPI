0eaf52ab7acfec23c219c29f3b04999d1067622e
cmkaylor <97851738+cmkaylor@users.noreply.github.com>
2022-01-17 18:43:25 -0600

    Add project files.

---
+++ PokerCardGameAPI/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace PokerCardGameAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.stack2 = new System.Windows.Forms.PictureBox();
+            this.playerCardTwo = new System.Windows.Forms.PictureBox();
+            this.stack1 = new System.Windows.Forms.PictureBox();
+            this.playerCardOne = new System.Windows.Forms.PictureBox();
+            this.stack3 = new System.Windows.Forms.PictureBox();
+            this.stack4 = new System.Windows.Forms.PictureBox();
+            this.stack5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.stack2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardTwo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 163);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stack2
+            // 
+            this.stack2.BackColor = System.Drawing.Color.Green;
+            this.stack2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stack2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stack2.Location = new System.Drawing.Point(229, 40);
+            this.stack2.Name = "stack2";
+            this.stack2.Size = new System.Drawing.Size(164, 209);
+            this.stack2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.stack2.TabIndex = 1;
+            this.stack2.TabStop = false;
+            this.stack2.Tag = "3";
+            // 
+            // playerCardTwo
+            // 
+            this.playerCardTwo.BackColor = System.Drawing.Color.Green;
+            this.playerCardTwo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.playerCardTwo.Location = new System.Drawing.Point(513, 363);
+            this.playerCardTwo.Name = "playerCardTwo";
+            this.playerCardTwo.Size = new System.Drawing.Size(164, 212);
+            this.playerCardTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerCardTwo.TabIndex = 2;
+            this.playerCardTwo.TabStop = false;
+            // 
+            // stack1
+            // 
+            this.stack1.BackColor = System.Drawing.Color.Green;
+            this.stack1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stack1.Location = new System.Drawing.Point(59, 40);
+            this.stack1.Name = "stack1";
+            this.stack1.Size = new System.Drawing.Size(164, 209);
+            this.stack1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.stack1.TabIndex = 3;
+            this.stack1.TabStop = false;
+            this.stack1.Tag = "2";
+            // 
+            // playerCardOne
+            // 
+            this.playerCardOne.BackColor = System.Drawing.Color.Green;
+            this.playerCardOne.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.playerCardOne.Location = new System.Drawing.Point(289, 366);
+            this.playerCardOne.Name = "playerCardOne";
+            this.playerCardOne.Size = new System.Drawing.Size(164, 209);
+            this.playerCardOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerCardOne.TabIndex = 4;
+            this.playerCardOne.TabStop = false;
+            // 
+            // stack3
+            // 
+            this.stack3.BackColor = System.Drawing.Color.Green;
+            this.stack3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stack3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stack3.Location = new System.Drawing.Point(399, 40);
+            this.stack3.Name = "stack3";
+            this.stack3.Size = new System.Drawing.Size(164, 209);
+            this.stack3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.stack3.TabIndex = 5;
+            this.stack3.TabStop = false;
+            this.stack3.Tag = "4";
+            // 
+            // stack4
+            // 
+            this.stack4.BackColor = System.Drawing.Color.Green;
+            this.stack4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stack4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stack4.Location = new System.Drawing.Point(569, 40);
+            this.stack4.Name = "stack4";
+            this.stack4.Size = new System.Drawing.Size(164, 209);
+            this.stack4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.stack4.TabIndex = 6;
+            this.stack4.TabStop = false;
+            this.stack4.Tag = "5";
+            // 
+            // stack5
+            // 
+            this.stack5.BackColor = System.Drawing.Color.Green;
+            this.stack5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.stack5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.stack5.Location = new System.Drawing.Point(739, 40);
+            this.stack5.Name = "stack5";
+            this.stack5.Size = new System.Drawing.Size(164, 209);
+            this.stack5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.stack5.TabIndex = 7;
+            this.stack5.TabStop = false;
+            this.stack5.Tag = "6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(995, 605);
+            this.Controls.Add(this.stack5);
+            this.Controls.Add(this.stack4);
+            this.Controls.Add(this.stack3);
+            this.Controls.Add(this.playerCardOne);
+            this.Controls.Add(this.stack1);
+            this.Controls.Add(this.playerCardTwo);
+            this.Controls.Add(this.stack2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Poker";
+            ((System.ComponentModel.ISupportInitialize)(this.stack2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardTwo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stack5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox stack2;
+        private System.Windows.Forms.PictureBox playerCardTwo;
+        private System.Windows.Forms.PictureBox stack1;
+        private System.Windows.Forms.PictureBox playerCardOne;
+        private System.Windows.Forms.PictureBox stack3;
+        private System.Windows.Forms.PictureBox stack4;
+        private System.Windows.Forms.PictureBox stack5;
+    }
+}
+
---
+++ PokerCardGameAPI/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PokerCardGameAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateCards();
+        }
+
+        private string deckID;
+        private List<Card> theCards = new List<Card>();
+        private int TurnNumber = 3;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonClick();
+        }
+
+        private void ButtonClick()
+        {
+            if (TurnNumber < 7)
+            {
+                foreach (Control control in this.Controls)
+                {
+                    if ((string)control.Tag == TurnNumber.ToString())
+                    {
+                        control.BackgroundImage = CardImage(theCards[TurnNumber].image);
+                    }
+                }
+
+                TurnNumber++;
+            }
+
+            else
+            {
+                foreach (Control control in this.Controls)
+                {
+                    if ((string)control.Tag == "3" || (string)control.Tag == "4" || (string)control.Tag == "5" || (string)control.Tag == "6")
+                    {
+                        control.BackgroundImage = null;
+                        control.BackColor = Color.Green;
+                    }
+                }
+                TurnNumber = 3;
+                GenerateCards();
+            }
+        }
+        private void GenerateCards()
+        {
+            string s = GetJson("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
+            GenRoot deserializedGenDeck = JsonConvert.DeserializeObject<GenRoot>(s);
+
+            deckID = deserializedGenDeck.deck_id;
+
+            string s2 = GetJson($"https://deckofcardsapi.com/api/deck/{deckID}/draw/?count=7");
+            Root deserializedDeck = JsonConvert.DeserializeObject<Root>(s2);
+            theCards = deserializedDeck.cards;
+
+            playerCardOne.Image = CardImage(theCards[0].image);
+            playerCardTwo.Image = CardImage(theCards[1].image);
+            stack1.Image = CardImage(theCards[2].image);
+        }
+
+        private Image CardImage(string url)
+        {
+            WebClient wc = new WebClient();
+            byte[] bytes = wc.DownloadData(url);
+            MemoryStream ms = new MemoryStream(bytes);
+            System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
+
+            return img;
+        }
+
+        private string GetJson(string url)
+        {
+            string returnable = "";
+            using (WebClient client = new WebClient())
+            {
+                returnable = client.DownloadString(url);
+            }
+
+            return returnable;
+        }
+    }
+}
